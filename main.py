0a7c207fd7aede1684913ca50e0849dcaef18fdc
Wolf <caffeinate@msn.com>
2022-08-16 22:36:15 -0400

    Single helix implementation working

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
 import dna_nanotube_tools
 from PyQt6.QtWidgets import QApplication
+import pyqtgraph as pg
 import sys
 
-interior_angle_multiples = [9] * 14
-switch_angle_multiples = [0] * 14
+interior_angle_multiples = [4]
+switch_angle_multiples = [0]
 
+interbase_angle = 2 * (360 / 21)
+side_view = dna_nanotube_tools.plot.side_view(
+    interior_angle_multiples, 3.38, interbase_angle, 12.6, 2.3
+)
+
+xs_NEMid = side_view.xs(1000, NEMid=False)[0][0]
+zs_NEMid = side_view.zs(1000, NEMid=False)[0][0][:-1]
+
+ui = pg.plot(
+    xs_NEMid,
+    zs_NEMid,
+    title="Top View of DNA",
+    symbol="o",
+    symbolSize=5,
+    pxMode=True,
+)
+ui.setAspectLocked(lock=True, ratio=50)
+ui.showGrid(x=True, y=True)
 
 def visualize_widget(widget):
     app = QApplication(sys.argv)
     widget.show()
     app.exec()
 
-
-# initilize top_view object
-top_view = dna_nanotube_tools.plot.top_view(
-    interior_angle_multiples, switch_angle_multiples, 2.3
-)
-
-# print out the cords
-cords = top_view.cords()
-
-# display the widget as a window
-visualize_widget(top_view.ui())
+widget = pg.GraphicsLayoutWidget()
+widget.setWindowTitle("Side View of DNA")
+visualize_widget(widget)
