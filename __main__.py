d0296bf7fe3d3179cf50c5a4b630ee164a14da6b
Wolf <caffeinate@msn.com>
2022-09-24 23:57:47 -0400

    Enabled icon

--- __main__.py
+++ __main__.py
@@ -1,10 +1,18 @@
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
 import sys
 import ui
+
+if sys.platform.startswith("win"):
+    # to get icon to work properly on windows this code must be run
+    # https://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7/1552105#1552105
+    import ctypes
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(__name__)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
+    app.setWindowIcon(QIcon("resources/icon.ico"))
     screen = app.primaryScreen()
     window = ui.main_window()
     window.show()
