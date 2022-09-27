3ad27512662fbf4083a0728768dd4a092637a76e
Wolf <caffeinate@msn.com>
2022-09-27 15:08:00 -0400

    Removed unused files

--- database/settings.py
+++ database/settings.py
@@ -1,12 +1,50 @@
 import pickle
 from types import SimpleNamespace
-import ui.datatypes
+from dataclasses import dataclass
+from typing import Union
+
 
 filename = "settings.nano"
 
+@dataclass
+class profile:
+    """
+    A settings profile.
+
+    Attributes:
+        count (int): Number of NEMids to initially generate per strand.
+        diameter (float): Diameter of a domain.
+        H (float): Height of a turn.
+        T (float): There are T turns every B bases.
+        B (float): There are B bases every T turns.
+        Z_c (float): Characteristic height.
+        Z_s (float): Switch height.
+        Z_b (float): Base height.
+        theta_b (float): Base angle.
+        theta_c (float): Characteristic angle.
+        theta_s (float): Switch angle.
+    """
+
+    count: int = 300
+    diameter: float = 0
+    H: float = 0.0
+    T: int = 0
+    B: int = 0
+    Z_c: float = 0.0
+    Z_b: Union[float, None] = None
+    Z_s: float = 0.0
+    theta_b: float = 0.0
+    theta_c: float = 0.0
+    theta_s: float = 0.0
+
+    def __post_init__(self):
+        if self.Z_b is None:
+            self.Z_b = (self.T * self.H) / self.B
+
+
 profiles = {}
 
-current = ui.datatypes.profile(
+current = profile(
     count=200,
     diameter=2.2,
     H=3.549,
--- ui/window.py
+++ ui/window.py
@@ -6,12 +6,36 @@ from PyQt6.QtWidgets import (
     QMenuBar,
     QVBoxLayout,
     QGroupBox,
+    QWidget
 )
-import ui.widgets, ui.widgets.config, ui.helpers
+import ui.widgets, ui.widgets.config
 from PyQt6.QtCore import Qt
 from contextlib import suppress
 import database.references
 
+def unrestrict_scale_upon_float(
+    widget: QWidget,
+    initial_width: int = 9999,
+    unbounded_width: int = 9999,
+    initial_height: int = 9999,
+    unbounded_height: int = 9999,
+):
+    """
+    Enable scaling beyond a dockable widget's normal maximum when it begins floating.
+
+    Args:
+        widget (QWidget): Widget to change size limitations upon floating/not floating of.
+        initial_width (int): Maximum widget width when not floating (in pixels).
+        unbounded_width (int): Maximum widget width when floating (in pixels).
+        initial_height (i)nt: Maximum widget height when not floating (in pixels).
+        unbounded_height (int): Maximum widget height when floating (in pixels).
+    """
+    if widget.isFloating():
+        widget.setMaximumWidth(unbounded_width)
+        widget.setMaximumHeight(unbounded_height)
+    else:
+        widget.setMaximumWidth(initial_width)
+        widget.setMaximumHeight(initial_height)
 
 class window(QMainWindow):
     """
@@ -65,7 +89,7 @@ class window(QMainWindow):
         docked_config.setMaximumWidth(215)
         # when this widget floats allow it to scale up to 400px wide
         docked_config.topLevelChanged.connect(
-            lambda: ui.helpers.unrestrict_scale_upon_float(
+            lambda: unrestrict_scale_upon_float(
                 self.config, initial_width=215, unbounded_width=460
             )
         )
@@ -98,7 +122,7 @@ class window(QMainWindow):
 
         # when this widget floats remove width scaling limitation
         docked_top_view.topLevelChanged.connect(
-            lambda: ui.helpers.unrestrict_scale_upon_float(
+            lambda: unrestrict_scale_upon_float(
                 self.top_view, initial_width=340
             )
         )
