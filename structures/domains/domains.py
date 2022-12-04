35cbf5b969fc040085e4cffe65f17b4b277b43f0
Wolf <caffeinate@msn.com>
2022-12-03 23:16:19 -0500

    Moved Subunit to subunit.py

--- structures/domains/domains.py
+++ structures/domains/domains.py
@@ -5,6 +5,7 @@ from typing import List, Iterable
 from constants.directions import *
 from helpers import inverse
 from structures.domains import Domain
+from structures.domains.subunit import Subunit
 
 logger = logging.getLogger(__name__)
 
@@ -21,17 +22,17 @@ class Domains:
     """
 
     def __init__(
-        self, domains: Iterable, symmetry: int, auto_antiparallel: bool
+        self, domains: Iterable, symmetry: int, antiparallel: bool
     ) -> None:
         """
-        Initialize a domains container.
+        Initialize a Domains container.
 
         Args:
             domains: A list of domains of a single subunit.
             symmetry: The symmetry type. Also known as "R".
         """
         self.subunit = Subunit(list(domains))
-        self.auto_antiparallel = auto_antiparallel
+        self.antiparallel = antiparallel
         self.symmetry = symmetry
 
     @property
@@ -52,7 +53,7 @@ class Domains:
             domain.index = index
 
         # force antiparallelity if self.autoparallel
-        if self.auto_antiparallel:
+        if self.antiparallel:
             direction = UP
             for index, domain in enumerate(output):
                 output[index].helix_joints = [direction, direction]
@@ -64,51 +65,3 @@ class Domains:
     def count(self) -> int:
         """Total number of domains."""
         return self.subunit.count * self.symmetry
-
-
-class Subunit:
-    """
-    A domain subunit.
-
-    Attributes:
-        domains: The domains in the subunit.
-        count: The number of domains in the subunit.
-    """
-
-    def __init__(self, domains: List[Domain]) -> None:
-        """
-        Create an instance of a subunit container.
-
-        Args:
-            domains: The domains in the subunit.
-        """
-        assert isinstance(domains, list)
-        self.domains = domains
-
-    @property
-    def count(self) -> int:
-        """Number of domains in the subunit."""
-        return len(self.domains)
-
-    @count.setter
-    def count(self, new):
-        """Change the number of domains in the subunit."""
-        # if the subunit count has decreased then trim off extra domains
-        if new < self.count:
-            self.domains = self.domains[:new]
-        # if the subunit count has increased then add placeholder domains based on last domain in domain list
-        else:
-            i = 0
-            while self.count < new:
-                previous_domain = self.domains[-1]
-                # the new template domains will be of altering strand directions with assumed
-                # strand switches of 0
-                self.domains.append(
-                    Domain(
-                        i,
-                        previous_domain.theta_interior_multiple,
-                        [inverse(previous_domain.helix_joints[1])] * 2,
-                        previous_domain.count,
-                    )
-                )
-                i += 1
