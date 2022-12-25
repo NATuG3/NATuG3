cc3f947662a01b8fe5cabd9aee80ed9f80bbc4a5
Wolf <caffeinate@msn.com>
2022-12-25 18:53:55 -0500

    Added assign_junctability method
    Took 4 minutes

--- structures/helices/double_helices.py
+++ structures/helices/double_helices.py
@@ -1,7 +1,6 @@
 from typing import List
 
-from structures.domains import Domain
-from structures.helices import DoubleHelix
+import settings
 
 
 class DoubleHelices:
@@ -17,29 +16,29 @@ class DoubleHelices:
 
     __slots__ = "double_helices"
 
-    def __init__(
-        self, double_helices: List[DoubleHelix] = None, domains: List[Domain] = None
-    ) -> None:
+    def __init__(self, items: List["Domain"] | List["DoubleHelix"]) -> None:
         """
         Create a double helices container.
 
         Args:
-            double_helices: A list of DoubleHelix objects.
-            domains: A list of domains to assign to the double helices. This should
-                not be provided if double_helices is provided. If this is provided, the
-                double helices will be automatically created.
+            items: Either a list of domains or a list of double helices. If it is a
+                list of domains then the double helices will be automatically created.
         """
-        self.double_helices = double_helices or [
-            DoubleHelix(domain) for domain in domains
-        ]
+        from structures.domains import Domain
+        from structures.helices import DoubleHelix
+
+        if isinstance(items[0], Domain):
+            self.double_helices = [DoubleHelix(domain) for domain in items]
+        else:
+            self.double_helices = items
 
     def __len__(self) -> int:
         return len(self.double_helices)
 
-    def __getitem__(self, index: int) -> DoubleHelix:
+    def __getitem__(self, index: int) -> "DoubleHelix":
         return self.double_helices[index]
 
-    def domains(self) -> List[Domain]:
+    def domains(self) -> List["Domain"]:
         """
         Obtain all the domains of all the double helices in their respective order.
 
@@ -48,38 +47,30 @@ class DoubleHelices:
         """
         return [double_helix.domain for double_helix in self.double_helices]
 
-    def assign_junctability(self) -> None:
+    def assign_junctability(self, closed: bool = True) -> None:
         """
         Assign juncmates and junctability for all NEMids contained in all helices.
+
+        Args:
+            closed: Whether to assume the tube is closed or not.
         """
+        # Create a list of all the domains
+        domains = self.domains()
+
         # Iterate through all the double helices
         for index, double_helix in enumerate(self.double_helices):
-            # To determine the next zeroed strand's direction we either look at the next
-            # strand's zeroed strand, or we wrap back to the first domain if we have
-            # reached the last domain (since cross-screen junctions are perfectly
-            # acceptable.
-            if index != len(self.domains()) - 1:
-                next_strand_index = index + 1
-            else:
-                next_strand_index = 0
-
-            # The zeroed strand directions for the current and next strand
-            current_zeroed_strand_direction = domains[domain.index].left_helix_joint
-            next_zeroed_strand_direction = domains[next_strand_index].left_helix_joint
-
             # The actual current domain's strands and the next domain's strands. Note
             # that "strand_pair" here means that it is a tuple of two strands,
             # one being up, and the other being down.
-            current_zeroed_strand = strands[domain.index][
-                current_zeroed_strand_direction
-            ]
-            next_zeroed_strand = strands[next_strand_index][
-                next_zeroed_strand_direction
-            ]
+            current_zeroed_strand = double_helix.zeroed_strand
+            next_zeroed_strand = self.double_helices[
+                index + 1 if index + 1 != len(domains) else 0
+            ].zeroed_strand
 
-            for item1 in current_zeroed_strand.items:
-                for item2 in next_zeroed_strand.items:
-
+            for item1 in current_zeroed_strand.NEMids():
+                for item2 in next_zeroed_strand.NEMids():
+                    # Create a function that will assign the juncmate and junctability
+                    # of the two NEMids if they need to be assigned.
                     def junct():
                         """Assign junctability and juncmates to the items."""
                         # Set junctability
@@ -96,7 +87,7 @@ class DoubleHelices:
                         continue
                     elif (x_dist := abs(item1.x_coord - item2.x_coord)) > len(
                         domains
-                    ) - 0.1 and self.closed():
+                    ) - 0.1 and closed:
                         junct()
                     elif x_dist < settings.junction_threshold:
                         junct()
