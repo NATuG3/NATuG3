ae43aaed7c2cea7826aef8af23a14eda8fdf5b64
Wolf <caffeinate@msn.com>
2023-02-17 11:30:59 -0500

    Initialization based on domains passed

--- structures/helices/double_helices.py
+++ structures/helices/double_helices.py
@@ -1,40 +1,93 @@
+from math import ceil
 from typing import List
+
+import numpy as np
+from numpy import argmax
+
+from constants.directions import DOWN, UP
+from structures.domains import Domains
+from structures.helices import DoubleHelix
+from structures.helices.helix import Helix
+from utils import inverse
 
 
 class DoubleHelices:
     """
-    A container for many DoubleHelix objects.
+    A container for multiple double helices.
 
-    This class provides methods to automatically determining matching items,
-    set junctability, and more.
+    A Helix's parent is a DoubleHelix. A DoubleHelix's parent is a DoubleHelices.
+
+    This class is able to compute point data for each helix, based on its relation to
+    the adjacent helices.
 
     Attributes:
-        double_helices (list): A list of DoubleHelix objects.
+        double_helices: A list of DoubleHelix objects.
+        nucleic_acid_profile: The nucleic acid profile to use for computations.
+
+    Methods:
+        domains: Obtain all the domains of all the double helices in their respective
+            order.
+        compute: Compute the point data for each helix. The data will be stored in the
+            helices respective x coord, z coord, and angle arrays.
     """
 
-    __slots__ = "double_helices"
+    __slots__ = "double_helices", "nucleic_acid_profile"
 
-    def __init__(self, items: List["Domain"] | List["DoubleHelix"]) -> None:
+    def __init__(self, domains: Domains, nucleic_acid_profile) -> None:
         """
-        Create a double helices container.
+        Initialize a container for DoubleHelix objects.
+
+        The size to make the data arrays of each helix is determined based on the
+        domains' GenerationCounts.
 
         Args:
-            items: Either a list of domains or a list of double helices. If it is a
-                list of domains then the double helices will be automatically created.
+            domains: A list of Domain objects.
+            nucleic_acid_profile: The nucleic acid profile to use for computations.
         """
-        from structures.domains import Domain
-        from structures.helices import DoubleHelix
+        self.double_helices = np.ndarray((len(domains),), dtype=DoubleHelix)
+        for index, domain in enumerate(domains.domains()):
+            # When we instantiate a DoubleHelix, we need to pass an up and a down
+            # helix, since every double helix must have exactly one of each. However,
+            # the user inputs the generation counts (number of points to generate) for
+            # the given helices based on the zeroedness of the helices--that is,
+            # which helix is lined up to the right helix joint direction helix of the
+            # previous domain.
 
-        if isinstance(items[0], Domain):
-            self.double_helices = [DoubleHelix(domain) for domain in items]
-        else:
-            self.double_helices = items
+            # Determine whether the zeroed helix is the up or down helix, and then
+            # fetch the sum of the generation count for the helix. Then create the helix.
+            zeroed_helix_direction = domain.left_helix_joint
+            zeroed_helix_count = sum(domain.count_by_direction(zeroed_helix_direction))
+            zeroed_helix = Helix(
+                direction=zeroed_helix_direction, size=zeroed_helix_count, domain=domain
+            )
+
+            # Repeat the same steps as above for the helix with the opposite direction.
+            other_helix_direction = inverse(zeroed_helix_direction)
+            other_helix_count = sum(domain.count_by_direction(other_helix_direction))
+            other_helix = Helix(
+                direction=other_helix_direction, size=other_helix_count, domain=domain
+            )
+
+            # Create references to the helices based on their direction.
+            up_helix = zeroed_helix if zeroed_helix.direction == UP else other_helix
+            down_helix = zeroed_helix if zeroed_helix.direction == DOWN else other_helix
+
+            # Create and store a DoubleHelix object that contains the two helices.
+            self.double_helices[index] = DoubleHelix(domain, up_helix, down_helix)
+
+        self.nucleic_acid_profile = nucleic_acid_profile
 
     def __len__(self) -> int:
         return len(self.double_helices)
 
     def __getitem__(self, index: int) -> "DoubleHelix":
         return self.double_helices[index]
+
+    def __setitem__(self, index: int, value: DoubleHelix):
+        self.double_helices[index] = value
+
+    def __iter__(self) -> "DoubleHelix":
+        return iter(self.double_helices)
 
     def domains(self) -> List["Domain"]:
         """
@@ -45,37 +98,43 @@ class DoubleHelices:
         """
         return [double_helix.domain for double_helix in self.double_helices]
 
-    def assign_junctability(self) -> None:
+    def compute(self):
         """
-        Assign juncmates and junctability for all NEMids contained in all helices.
+        Compute the point data for each helix.
 
-        Args:
-            closed: Whether to assume the tube is closed or not.
+        This computes the x coord, z coord, and angle arrays for each helix. The data
+        is stored in the helices respective x coord, z coord, and angle arrays.
         """
-        # Create a list of all the domains
-        domains = self.domains()
+        # Each domain (except the zeroth domain) is lined up such that the left side
+        # of the domain lines up exactly with NEMids of the right side of the
+        # previous domain; however, each domain still has two helices.
 
-        from structures.points import NEMid
+        # What we will do is compute the NEMids for the side of the domain that makes
+        # a connection with the previous domain, and then we will compute the NEMids
+        # for the other side of the domain later.
 
-        # Iterate through all the double helices
-        for index, double_helix in enumerate(self.double_helices):
-            # The actual current domain's strands and the next domain's strands. Note
-            # that "strand_pair" here means that it is a tuple of two strands,
-            # one being up, and the other being down.
-            current_zeroed_strand = double_helix.zeroed_helix
-            next_zeroed_strand = self.double_helices[
-                index + 1 if index + 1 != len(domains) else 0
-            ].zeroed_helix
+        for index, double_helix in enumerate(self):
+            # Create a reference to the previous double helix
+            previous_double_helix = self[index - 1]
 
-            for item1 in current_zeroed_strand.items.by_type(NEMid):
-                for item2 in next_zeroed_strand.items.by_type(NEMid):
-                    # Perform a preliminary z coord check since dist() is much more
-                    # computationally intensive, and it is best to avoid it if possible
-                    if item1.overlaps(item2, len(self.double_helices)):
-                        # Set junctability
-                        item1.junctable = True
-                        item2.junctable = True
+            if index == 0:
+                # The first domain is a special case. The z coord of the first NEMid
+                # of the first domain is 0.
+                initial_z_coord = 0
+            else:
+                # The initial z coord for all domains except the zeroth domain is the
+                # z coordinate of the right-most point of the previous double helix's
+                # right joint helix.
+                initial_z_coord = previous_double_helix.right_helix.z_coords[
+                    argmax(previous_double_helix.right_helix.x_coords)
+                ]
 
-                        # Set juncmates
-                        item1.juncmate = item2
-                        item2.juncmate = item1
+                # Shift down the initial z coord. We can shift it down in increments
+                # of Z_b * B, which we will call the "decrease_interval" (the
+                # interval at which the z coord decreases).
+                decrease_interval = (
+                    self.nucleic_acid_profile.Z_b * self.nucleic_acid_profile.B
+                )
+                initial_z_coord -= (
+                    ceil(initial_z_coord / decrease_interval) * decrease_interval
+                )
