a0b94e0936141859c5ec31c4d1e469811a7ba456
Wolf <caffeinate@msn.com>
2023-02-22 14:11:14 -0500

    Removed customized __setattr__ method
    Took 16 seconds

--- structures/helices/helix.py
+++ structures/helices/helix.py
@@ -1,7 +1,6 @@
 import itertools
 from dataclasses import dataclass, field
-from types import NoneType
-from typing import Literal, Type, Iterable
+from typing import Literal, Type
 
 import numpy as np
 
@@ -30,31 +29,6 @@ class HelixData:
     angles: np.ndarray | None = None
 
     _data_arrays = ("x_coords", "z_coords", "angles")
-
-    def __setattr__(self, key, value):
-        """
-        Set an attribute of the helix data.
-
-        When a data array is set up, the parent helix's sequence is set to an empty
-        array of the same size as the data array.
-        """
-        # Perform special processing if they are setting a data array.
-        if key in self._data_arrays:
-            # In initialization of the data arrays they are set to None, so we must
-            # allow it.
-            if value is None:
-                super(HelixData, self).__setattr__(key, value)
-            # If the value is a numpy array, we will set the sequence of the helix to
-            # an array of the same size as the data array.
-            else:
-                if isinstance(value, np.ndarray):
-                    self.helix.sequence = np.full(len(value), "X")
-                    super(HelixData, self).__setattr__(key, value)
-                else:
-                    raise TypeError("The data arrays must be numpy arrays.")
-        # Otherwise, just set the attribute.
-        else:
-            super(HelixData, self).__setattr__(key, value)
 
     def size(self) -> int:
         """
@@ -92,16 +66,11 @@ class Helix:
         domain: The domain that this helix lies within.
         data: The data of the helix. This is a HelixData object, and contains the
             x-coordinates, z-coordinates, and angles of the points in the helix.
-        sequence: The sequence of the nucleosides of the helix. This starts out as
-            None, and is automatically made an empty numpy array of the same size as
-            the data arrays when the data arrays are set.
     """
 
     direction: Literal[UP, DOWN]
     double_helix: Type["DoubleHelix"] | None
     data: HelixData = field(default_factory=HelixData)
-
-    _sequence: np.ndarray = field(default=None, init=False)
 
     def __post_init__(self):
         self.data.helix = self
@@ -112,40 +81,6 @@ class Helix:
             len(self.data.x_coords) == len(self.data.z_coords) == len(self.data.angles)
         )
         return len(self.data.angles)
-
-    @property
-    def sequence(self) -> np.ndarray | None:
-        """
-        The sequence of the nucleosides of the helix.
-
-        This starts out as None, and is automatically made an empty numpy array of
-        the same size as the data arrays when the data arrays are set. The empty
-        array uses the letter "X" to represent unset bases.
-        """
-        return self._sequence
-
-    @sequence.setter
-    def sequence(self, value: Iterable[str] | None):
-        """
-        Set the sequence of the helix or clear the sequence.
-
-        Args:
-            value: The sequence of the helix. This must be an iterable of capital
-                valid base strings or None. If None, the sequence is cleared.
-
-        Raises:
-            TypeError: If the sequence is not an iterable or None.
-        """
-        if not isinstance(value, (Iterable, NoneType)):
-            raise TypeError("The sequence must be an iterable or None.")
-
-        if value is None:
-            self._sequence = np.full(self.data.size(), "X")
-        else:
-            if isinstance(value, np.ndarray):
-                self._sequence = value
-            else:
-                self._sequence = np.array(value)
 
     @property
     def domain(self):
