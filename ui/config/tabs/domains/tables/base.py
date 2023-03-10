12cc58133aed14098098a74234f6c80e28b87696
Wolf <caffeinate@msn.com>
2023-03-10 16:09:15 -0500

    Created base class for domains panel tables

---
+++ ui/config/tabs/domains/tables/base.py
@@ -0,0 +1,96 @@
+from typing import List, Tuple
+
+from PyQt6.QtCore import QEvent, Qt
+from PyQt6.QtGui import QKeyEvent
+from PyQt6.QtWidgets import QTableWidget, QHeaderView, QAbstractItemView, QApplication
+
+
+class DomainsBaseTable(QTableWidget):
+    """
+    The parent table class for all tables in the domains tab.
+
+    This table class has styles and tabbing events all set up.
+    """
+
+    def __init__(self, parent, headers: List[str]) -> None:
+        super().__init__(parent)
+        self.top_headers = tuple(headers)
+        self._headers()
+        self._prettify()
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        """Intercept a keypress to alter tabbing."""
+        if event.key() in (
+            Qt.Key.Key_Tab,
+            Qt.Key.Key_Backtab,
+            Qt.Key.Key_Down,
+            Qt.Key.Key_Up,
+        ):
+            # Determine the row and column to highlight
+            row, column = self.currentRow(), self.currentColumn()
+            if event.key() in (Qt.Key.Key_Tab, Qt.Key.Key_Down):
+                row += 1
+            else:
+                row -= 1
+            if row == len(self.rows):
+                row = 0
+
+            # Ensure that the previous widget's data is saved
+            self.cellWidget(row, column).editingFinished.emit()
+            self.cell_widget_updated.emit()
+
+            # Disable signals temporarily
+            self.setTabKeyNavigation(False)
+            self.blockSignals(True)
+
+            # Obtain the widget
+            to_focus = self.cellWidget(row, column)
+
+            # Create a list of key press events to send
+            if to_focus is not None:
+                # Change the table focus
+                self.setCurrentCell(row, column)
+                to_focus.setFocus()
+
+                # Simulate a control A press
+                QApplication.postEvent(
+                    to_focus,
+                    QKeyEvent(
+                        QEvent.Type.KeyPress,
+                        Qt.Key.Key_A,
+                        Qt.KeyboardModifier.ControlModifier,
+                    ),
+                )
+
+            # Unblock signals
+            self.blockSignals(False)
+            self.setTabKeyNavigation(True)
+        else:
+            # Otherwise use the normal keypress event
+            super().keyPressEvent(event)
+
+    def _headers(self):
+        """Configure top headers of widget"""
+        # Create a column for each header
+        self.setColumnCount(len(self.top_headers))
+        # Apply the headers
+        self.setHorizontalHeaderLabels(self.top_headers)
+
+    def _prettify(self):
+        """Style the domain panel."""
+        # Set the style worksheet of the panel
+        self.setStyleSheet("QTableView::item{padding: 3.25px; text-align: center}")
+
+        # Show table grid
+        self.setShowGrid(True)
+
+        # Enable smooth scrolling
+        self.setVerticalScrollMode(QAbstractItemView.ScrollMode.ScrollPerPixel)
+
+        # All columns are statically sized
+        for index, column in enumerate(range(self.columnCount())):
+            self.horizontalHeader().setSectionResizeMode(
+                index, QHeaderView.ResizeMode.ResizeToContents
+            )
+            # Set the column width to the header's length times two in pixels
+            # self.setColumnWidth(index, len(self.top_headers[index])*11)
