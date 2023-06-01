1db2535baf51a25fdcac2e93f4ca59be275845f4
Wolf <caffeinate@msn.com>
2023-06-01 11:15:41 -0400

    Add inverted method

--- structures/domains/subunit.py
+++ structures/domains/subunit.py
@@ -26,6 +26,7 @@ class Subunit:
     Methods:
         append()
         remove()
+        inverted()
     """
 
     def __init__(
@@ -53,7 +54,7 @@ class Subunit:
 
         # assign the strands of all the domains to us
         for domain in self.domains:
-            domain.strands = self
+            domain.parent = self
 
         assert isinstance(domains, Iterable)
 
@@ -68,7 +69,7 @@ class Subunit:
             The domain will have its strands set to this subunit.
         """
         self.domains.append(domain)
-        domain.strands = self
+        domain.parent = self
 
     def remove(self, domain: "Domain") -> None:
         """
@@ -81,7 +82,7 @@ class Subunit:
             The domain will have its strands reset to None.
         """
         self.domains.remove(domain)
-        domain.strands = None
+        domain.parent = None
 
     def copy(self) -> "Subunit":
         """
@@ -97,6 +98,18 @@ class Subunit:
             template=self.template,
             parent=self.parent,
         )
+
+    def inverted(self) -> "Subunit":
+        """
+        Obtain an inverted copy of the subunit.
+
+        An inverted subunit is a subunit with all of its domains helix joint directions
+        inverted.
+
+        Returns:
+            A brand-new subunit object with brand-new domain objects.
+        """
+        return [domain.inverted() for domain in self.domains]
 
     @property
     def count(self) -> int:
@@ -150,6 +163,18 @@ class Subunit:
                     )
                 )
                 i += 1
+
+    def __getitem__(self, item):
+        """
+        Obtain a domain from the subunit.
+
+        Args:
+            item: The index of the domain to obtain.
+
+        Returns:
+            The domain at the given index.
+        """
+        return self.domains[item]
 
     def __repr__(self) -> str:
         """
